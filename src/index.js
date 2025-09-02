71e9e3ae50bdcbaac7c6d608852910073cf1c5ef
DanilinaKristina <kris.chachkova@gmail.com>
2025-09-02 22:29:23 +0300

    feat:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = bracketsConfig.map((pair) => pair[0]);
+  const closeBrackets = bracketsConfig.map((pair) => pair[1]);
+  const bracketMap = new Map();
+  bracketsConfig.forEach((pair) => {
+    const [open, close] = pair;
+    bracketMap.set(close, open);
+  });
+  str.split('').forEach((char) => {
+    const isOpen = openBrackets.includes(char);
+    const isClose = closeBrackets.includes(char);
+
+    if (isOpen && isClose) {
+      if (stack.length > 0 && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (isOpen) {
+      stack.push(char);
+    } else if (isClose) {
+      if (stack.length === 0) {
+        stack.push('invalid'); // чтобы не упасть, но сохранить false
+        return;
+      }
+      const last = stack.pop();
+      if (last !== bracketMap.get(char)) {
+        stack.push('invalid');
+      }
+    }
+  });
+
+  return stack.length === 0;
 };
